277c62550689ef38641f2d0fa38c282f268d4310
Daniel Espinal <dancancode@protonmail.com>
2022-06-03 13:49:24 -0400

    footer adjustments

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,47 @@
-import styles from "../styles/Home.module.css";
-import { Box, Flex, Icon } from "@chakra-ui/react";
-import { BsInstagram } from "react-icons/bs";
+import styles from "../public/Home.module.css";
+import Link from "next/link";
+import { Box, Center, Divider, Flex, Icon } from "@chakra-ui/react";
+import { FaInstagram, FaEnvelope, FaPinterest } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <Box
-      as="footer"
-      className={styles.footer}
-      align="center"
-      opacity={0.4}
-      fontSize="sm"
-    >
-      <Flex direction="column" justifyContent="center" alignItems="center">
-        <Flex marginBottom={3}>
+    <Box as="footer" className={styles.footer}>
+      <Flex className={styles.innerFooter}>
+        <Box>
           <a href="https://www.instagram.com/simplytogether.e/">
-            <Icon as={BsInstagram} color="#000000" boxSize={6} />
+            <Icon as={FaInstagram} boxSize={6} />
           </a>
-        </Flex>
 
-        <p>
-          &copy; {new Date().getFullYear()} Simply Togehter Events. All Rights
-          Reserved.
-        </p>
+          <a href="mailto:simplytogetherevents@gmail.com">
+            <Icon as={FaPinterest} boxSize={6} />
+          </a>
+
+          <a href="mailto:simplytogetherevents@gmail.com">
+            <Icon as={FaEnvelope} boxSize={6} />
+          </a>
+        </Box>
+
+        <Center height="3.5em">
+          <Divider
+            borderColor="#000000"
+            borderLeftWidth={2}
+            orientation="vertical"
+          />
+        </Center>
+
+        <Flex direction="column">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/services">Services</Link>
+          <Link href="/gallery">Gallery</Link>
+          <Link href="/contact">Contact</Link>
+        </Flex>
       </Flex>
+
+      <p>
+        &copy; {new Date().getFullYear()} Simply Togehter Events. All Rights
+        Reserved.
+      </p>
     </Box>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Layout from "../components/layout";
-import "../styles/globals.css";
+import "../public/globals.css";
 
 function Website({ Component, pageProps }) {
   return (
