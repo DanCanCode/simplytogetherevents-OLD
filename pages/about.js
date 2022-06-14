115a0cffc7d87aab986e6f54218fde0c7701217b
Daniel Espinal <dancancode@protonmail.com>
2022-06-14 18:21:28 -0400

    about page almost done

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import { Box, Container, Text, Image } from "@chakra-ui/react";
+import { Box, Container, Text, Image, Divider, Center } from "@chakra-ui/react";
 import styles from "../public/Home.module.css";
 
 const About = (props) => {
@@ -73,8 +73,61 @@ const About = (props) => {
         <Text>social media links maybe</Text>
       </Container>
 
-      <Box>
-        <Text>FAQ SECTION</Text>
+      <Center>
+        <Divider
+          width="70vw"
+          borderColor="rgba(0,0,0, 0.8)"
+          borderBottomWidth={2}
+        />
+      </Center>
+
+      <Box padding="2em" bgColor="#f4f4f4">
+        <Text textAlign="center" fontFamily="serif" fontSize={32}>
+          {"Frequently Asked Questions & Information"}
+        </Text>
+
+        <Box className={styles.faq}>
+          <Box>
+            <Text fontFamily="serif" fontSize={22}>
+              what if I need to cancel or reschedule my luxury picnic?
+            </Text>
+            <Text>
+              Things come up, thats life. In the event that you may need to
+              cancel or reschedule, please give at least 72 hours notice. You
+              can reschedule your picnic for no extra charge if within the time
+              frame. Any event canceled or rescheduled less than 72 hours will
+              not be refunded.
+            </Text>
+          </Box>
+
+          <Box>
+            <Text fontFamily="serif" fontSize={22}>
+              what if I am late to my luxury picnic?
+            </Text>
+            <Text>
+              If you arrive late to your scheduled picnic time, we will not be
+              able to extend the time. Again, things come up and you can
+              reschedule with no charge if done within 72 hours in advance.
+              There will be no price adjustments or refunds if you arrive late.
+              If you do not show up, it will be considered a cancelation and no
+              refund will be issued.
+            </Text>
+          </Box>
+
+          <Box>
+            <Text fontFamily="serif" fontSize={22}>
+              what if there is inclement weather the day of my scheduled picnic?
+            </Text>
+            <Text>
+              We can't control the weather and neither can you! You will be able
+              to reschedule your picnic at no extra charge if the weather
+              decides to not be great. We also offer the option of changing the
+              picnic location to somewhere indoors (fees may apply) if you are
+              persistent on keeping the date. Either way, will make it happen,
+              stress free!
+            </Text>
+          </Box>
+        </Box>
       </Box>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
-import Image from "next/image";
 import styles from "../public/Home.module.css";
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Image } from "@chakra-ui/react";
 
-const Home = () => {
-  return <Box>Home</Box>;
+const Home = (props) => {
+  const Navbar = props.navbar;
+  const [width, height] = props.windowSize;
+  return (
+    <>
+      <Box className={styles.homeBackground}>
+        <Box w="100%" h="100vh" bgColor="rgba(0,0,0,0.5)">
+          {Navbar}
+          <Box className={styles.mainBackgroundText}>
+            <Image
+              htmlWidth={350}
+              paddingBottom="1em"
+              align="center"
+              src="./simplytogether_events.png"
+              alt="Simply Together Events Logo"
+            />
+            <Text
+              fontSize={20}
+              textTransform="uppercase"
+              letterSpacing={2}
+              maxWidth="40vw"
+            >
+              to simply bring everyone together the best way we know how.
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Home;
