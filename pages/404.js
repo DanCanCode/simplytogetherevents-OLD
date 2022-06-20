a8f541528468c469c3af51713d7e706eab38bca6
Daniel Espinal <dancancode@protonmail.com>
2022-06-20 13:59:24 -0400

    404 page update

--- pages/404.js
+++ pages/404.js
@@ -1,29 +1,70 @@
-import NextLink from "next/link";
+import Link from "next/link";
 import {
   Box,
-  Heading,
   Text,
+  Image,
   Container,
-  Divider,
+  Icon,
   Button,
+  IconButton,
 } from "@chakra-ui/react";
+import { FaInstagram, FaEnvelope, FaPinterest } from "react-icons/fa";
 
-const NotFound = () => {
+const NotFound = (props) => {
   return (
-    <Container>
-      <Heading as="h1" textAlign="center">
-        Not found
-      </Heading>
-      <Text my={2} textAlign="center">
-        The page you&apos;re looking for was not found.
-      </Text>
-      <Divider my={4} />
-      <Box my={6} align="center">
-        <NextLink href="/" passHref>
-          <Button colorScheme="teal">Return to home</Button>
-        </NextLink>
-      </Box>
-    </Container>
+    <Box backgroundColor="#fcf7f2">
+      <Container padding="3em">
+        <Image
+          src="./simplytogether_events.png"
+          alt="simply together events logo"
+        />
+        <Box paddingY="1em">
+          <Text textAlign="center" fontSize={42}>
+            404
+          </Text>
+          <Text textAlign="center" fontSize={24}>
+            Page Not Found
+          </Text>
+        </Box>
+
+        <Box align="center">
+          <Link href="/" passHref>
+            <Button colorScheme="pink" size="lg">
+              Return Home
+            </Button>
+          </Link>
+        </Box>
+
+        <Box align="center" paddingTop="1em">
+          <IconButton
+            marginX=".5em"
+            as="a"
+            href="https://www.instagram.com/simplytogether.e/"
+            colorScheme="purple"
+            borderRadius="100%"
+            icon={<Icon as={FaInstagram} />}
+          />
+
+          <IconButton
+            as="a"
+            marginX=".5em"
+            colorScheme="red"
+            borderRadius="100%"
+            href="https://www.pinterest.com/simplytogetherevents/_created"
+            icon={<Icon as={FaPinterest} />}
+          />
+
+          <IconButton
+            as="a"
+            marginX=".5em"
+            colorScheme="whatsapp"
+            borderRadius="100%"
+            href="mailto:simplytogetherevents@gmail.com"
+            icon={<Icon as={FaEnvelope} />}
+          />
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,6 @@
 import {
   Box,
   Text,
-  Image,
   Input,
   useToast,
   Select,
@@ -9,8 +8,6 @@ import {
   Button,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  Container,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import styles from "../public/Home.module.css";
@@ -26,19 +23,9 @@ const Contact = (props) => {
   });
 
   const Navbar = props.navbar;
-  const [width, height] = props.windowSize;
+  const Footer = props.footer;
 
   const toast = useToast();
-
-  const contactPhoto = () => {
-    if (width > 800) {
-      return (
-        <Box>
-          <Image src="./images/IMG_6003.jpg" alt="" />
-        </Box>
-      );
-    }
-  };
 
   const handleSubmit = () => {
     if (contactInfo.name.length) {
@@ -74,168 +61,130 @@ const Contact = (props) => {
   };
 
   return (
-    <>
+    <Box backgroundColor="#fcf7f2">
       <Box className={styles.contactBackground}>
-        <Box w="100%" h="100vh" bgColor="rgba(0,0,0,0.5)">
+        <Box backgroundColor="rgba(0,0,0, 0.4)">
           {Navbar}
-          <Box className={styles.mainBackgroundText}>
-            <Text
-              paddingBottom={2}
-              fontSize={48}
-              fontFamily="serif"
-              letterSpacing={1}
+
+          <Box className={styles.beforeContact}>
+            <Text letterSpacing={2}>LET'S GET PLANNING!</Text>
+
+            <Button
+              colorScheme="messenger"
+              _active={{
+                transform: "scale(0.9)",
+              }}
             >
-              Contact Us
-            </Text>
-            <Text fontSize={20} textTransform="uppercase" letterSpacing={2}>
-              Let's Do This!
-            </Text>
+              request appointment
+            </Button>
+          </Box>
+
+          <Box className={styles.contact}>
+            <FormControl isRequired className={styles.contactInputs}>
+              <Box>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <Input
+                  id="name"
+                  type="name"
+                  value={contactInfo.name}
+                  onChange={(event) => {
+                    setContactInfo((prevState) => ({
+                      ...prevState,
+                      name: event.target.value,
+                    }));
+                  }}
+                  variant="flushed"
+                />
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  value={contactInfo.email}
+                  onChange={(event) => {
+                    setContactInfo((prevState) => ({
+                      ...prevState,
+                      email: event.target.value,
+                    }));
+                  }}
+                  variant="flushed"
+                />
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="phone">Phone</FormLabel>
+                <Input
+                  id="phone"
+                  type="phone"
+                  value={contactInfo.phone}
+                  onChange={(event) => {
+                    setContactInfo((prevState) => ({
+                      ...prevState,
+                      phone: event.target.value,
+                    }));
+                  }}
+                  variant="flushed"
+                />
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="event">Event Type</FormLabel>
+                <Select
+                  variant="flushed"
+                  placeholder="Select a service"
+                  onChange={(event) => {
+                    setContactInfo((prevState) => ({
+                      ...prevState,
+                      event: event.target.value,
+                    }));
+                  }}
+                >
+                  <option value="Event Planning">Event Planning</option>
+                  <option value="Event Design">Event Design</option>
+                  <option value="Luxury Picnic">Luxury Picnic</option>
+                  <option value="Rental">Rental</option>
+                </Select>
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="info">Tell us about the event:</FormLabel>
+                <Textarea
+                  id="info"
+                  type="info"
+                  value={contactInfo.info}
+                  onChange={(event) => {
+                    setContactInfo((prevState) => ({
+                      ...prevState,
+                      info: event.target.value,
+                    }));
+                  }}
+                  variant="flushed"
+                  placeholder="Enter details about your event here"
+                  _placeholder={{ color: "#ffffff" }}
+                />
+              </Box>
+
+              <Button
+                variant="outline"
+                size="lg"
+                _active={{
+                  transform: "scale(0.9)",
+                }}
+                onClick={() => {
+                  handleSubmit();
+                }}
+              >
+                Submit
+              </Button>
+            </FormControl>
           </Box>
         </Box>
       </Box>
 
-      <Box className={styles.beforeContact}>
-        <Container>
-          <Text fontSize={22} textTransform="uppercase" letterSpacing={2}>
-            Are you ready?
-          </Text>
-          <Text fontFamily="serif" fontSize={18}>
-            We are here to help! Tell us about the event and let's make your
-            vision come to life. Please fill out the form below and we will
-            contact you to dicuss your ideas and/or confirm your booking.
-          </Text>
-          <Text color="rgba(0, 0, 0, 0.5)" fontSize={14} marginTop="1em">
-            * Please allow 1 to 3 business days for a response. Also, please
-            check your junk/spam mail.
-          </Text>
-        </Container>
-      </Box>
-
-      <Box className={styles.contact}>
-        <FormControl isRequired className={styles.contactInputs}>
-          <Box>
-            <FormLabel htmlFor="name" fontFamily="serif" fontSize={18}>
-              Name
-            </FormLabel>
-            <Input
-              id="name"
-              type="name"
-              borderColor="rgba(0, 0, 0, 0.5)"
-              value={contactInfo.name}
-              onChange={(event) => {
-                setContactInfo((prevState) => ({
-                  ...prevState,
-                  name: event.target.value,
-                }));
-              }}
-              variant="flushed"
-            />
-          </Box>
-
-          <Box>
-            <FormLabel htmlFor="email" fontFamily="serif" fontSize={18}>
-              Email
-            </FormLabel>
-            <Input
-              id="email"
-              type="email"
-              borderColor="rgba(0, 0, 0, 0.5)"
-              value={contactInfo.email}
-              onChange={(event) => {
-                setContactInfo((prevState) => ({
-                  ...prevState,
-                  email: event.target.value,
-                }));
-              }}
-              variant="flushed"
-            />
-          </Box>
-
-          <Box>
-            <FormLabel htmlFor="phone" fontFamily="serif" fontSize={18}>
-              Phone
-            </FormLabel>
-            <Input
-              id="phone"
-              type="phone"
-              borderColor="rgba(0, 0, 0, 0.5)"
-              value={contactInfo.phone}
-              onChange={(event) => {
-                setContactInfo((prevState) => ({
-                  ...prevState,
-                  phone: event.target.value,
-                }));
-              }}
-              variant="flushed"
-            />
-          </Box>
-
-          <Box>
-            <FormLabel htmlFor="event" fontFamily="serif" fontSize={18}>
-              Event Type
-            </FormLabel>
-            <Select
-              variant="flushed"
-              borderColor="rgba(0, 0, 0, 0.5)"
-              placeholder="Select a service"
-              onChange={(event) => {
-                setContactInfo((prevState) => ({
-                  ...prevState,
-                  event: event.target.value,
-                }));
-              }}
-            >
-              <option value="Event Planning">Event Planning</option>
-              <option value="Event Design">Event Design</option>
-              <option value="Luxury Picnic">Luxury Picnic</option>
-              <option value="Rental">Rental</option>
-            </Select>
-          </Box>
-
-          <Box>
-            <FormLabel htmlFor="info" fontFamily="serif" fontSize={18}>
-              Tell us about the event:
-            </FormLabel>
-            <Textarea
-              id="info"
-              type="info"
-              borderColor="rgba(0, 0, 0, 0.5)"
-              value={contactInfo.info}
-              onChange={(event) => {
-                setContactInfo((prevState) => ({
-                  ...prevState,
-                  info: event.target.value,
-                }));
-              }}
-              variant="flushed"
-              placeholder="Enter details about your event here"
-              _placeholder={{ opacity: 0.5, color: "#000000" }}
-            />
-          </Box>
-
-          <Button
-            bgColor="#FF0A54"
-            size="lg"
-            height="48px"
-            width="100px"
-            _hover={{
-              bgColor: "#C9184A",
-            }}
-            _active={{
-              bgColor: "#C9184A",
-              transform: "scale(0.9)",
-            }}
-            onClick={() => {
-              handleSubmit();
-            }}
-          >
-            <Text color="#ffffff">Submit</Text>
-          </Button>
-        </FormControl>
-
-        {contactPhoto()}
-      </Box>
-    </>
+      {Footer}
+    </Box>
   );
 };
 
